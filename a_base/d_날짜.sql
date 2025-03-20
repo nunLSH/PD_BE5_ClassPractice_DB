afe7eda7bb7d071892445a1bdf0348a559b817e1
imseohyeon <forestlight749@gmail.com>
2025-03-20 17:37:22 +0900

    (0320) 날짜

---
+++ a_base/d_날짜.sql
@@ -0,0 +1,12 @@
+# date
+insert into tb_type (
+	t_date, t_time, t_datetime, t_timestamp
+) values('25-03-20', '10:06'
+, '25-03-20 10:06:01'
+, '25-03-20T10:07:01'
+);
+
+select * from tb_type;
+set @@session.time_zone = '+01:00';
+commit;
+
